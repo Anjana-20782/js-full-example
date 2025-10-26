cf2f4907e08aca4eae39015af73ccc4786ba483c
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-26 11:11:55 +0530

    new contents added in js  added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
-function changeText() {
-      document.getElementById("message").innerHTML = "You clicked the button! 🎉";
-    }
+// 1
+
+// function changeText() {
+//       document.getElementById("message").innerHTML = "You clicked the button! 🎉";
+//     }
+
+
+
+//2
+
+ let a = 10;
+  let b = 20;
+  let sum = a + b;
+
+  document.getElementById("result").innerHTML = "The sum is: " + sum;
