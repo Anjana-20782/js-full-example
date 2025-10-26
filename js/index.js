5fb19ca9ebdc70fae3e005469687e7a5d8bcdad0
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-26 11:23:58 +0530

    new contents added in js  added

--- js/index.js
+++ js/index.js
@@ -18,8 +18,17 @@
 
 //3 you can declare same variable name again using var
 
-var name = "Anjana"; // Output: Anjana
-console.log(name);
+// var name = "Anjana"; // Output: Anjana
+// console.log(name);
 
-var names = "Sivadasan";  // Redeclared
-console.log(names); // Output: Sivadasan
+// var names = "Sivadasan";  // Redeclared
+// console.log(names); // Output: Sivadasan
+
+
+
+//4 if var is declared inside the block ,it can still be acessed outside the block
+
+{
+  var city = "Kochi";
+}
+console.log(city);                          //kochi
